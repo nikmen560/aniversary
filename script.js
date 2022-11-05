5f3a358b0673bd7d721094d1f21c05b04cdb88f5
Mikita Kavaliou <Kent720p@gmail.com>
2022-11-05 12:47:39 +0100

    added photos and append onclick

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
-$(":header").click(function(){
-    $("#container").append("<div id = 'heart'></div>")
+var i = 1;
+$(":header").click(function () {
+  $("#container").append("<div id = 'heart'></div>");
+  
+  if(i === 4) {
+    $("img").remove();
+  }
+  $("#photos").prepend(`<img src="./img/photo${i}.jpg" style="max-width:100%;height:40%;" alt=""></img>`);
+  i++;
+  
 
-    anime({
-  targets: '#heart',
-  scale: function(el, index) {
-    return anime.random(.3, .5); 
-  },
-      
-   rotate: {
-    value: function(el, index) {
-    return anime.random(-40, 40); 
-  },
-    duration: function(el, index) {
-    return anime.random(400, 2000); 
-  },
-    easing: 'easeInOutExpo'
-  },
-  delay: 50,
-  translateX: function(el, index) {
-    return anime.random(10, 50); 
-  },
-   translateY: function(el, index) {
-    return anime.random(10, 100); 
-  },  
-  direction: 'alternate',
-  elasticity:300,
-  loop: true
-       
-   });
+  anime({
+    targets: "#heart",
+    scale: function (el, index) {
+      return anime.random(0.3, 0.5);
+    },
 
-      $("#heart").css("left", Math.random() *260);
-        $("#heart").css("top", Math.random() *240);
-
+    rotate: {
+      value: function (el, index) {
+        return anime.random(-40, 40);
+      },
+      duration: function (el, index) {
+        return anime.random(400, 2000);
+      },
+      easing: "easeInOutExpo",
+    },
+    delay: 50,
+    translateX: function (el, index) {
+      return anime.random(10, 50);
+    },
+    translateY: function (el, index) {
+      return anime.random(10, 100);
+    },
+    direction: "alternate",
+    elasticity: 300,
+    loop: true,
   });
+
+  $("#heart").css("left", Math.random() * 260);
+  $("#heart").css("top", Math.random() * 240);
+
+});
